1b592266a92efe92dcdb4185d1a55439a197d402
eduardojose <eduardojose@ds.va.ciss.com.br>
2014-10-15 11:39:19 -0300

    retirar os métodos get dos objetos, pois estava causando erro ao subir o servidor

--- spring-forum/src/main/java/br/com/itexto/springforum/controladores/HomeController.java
+++ spring-forum/src/main/java/br/com/itexto/springforum/controladores/HomeController.java
@@ -26,8 +26,7 @@ import br.com.itexto.springforum.entidades.Usuario;
 
 @Controller
 public class HomeController {
-	
-	
+
 	@Autowired
 	private DAOUsuario daoUsuario;
 	@Autowired
@@ -36,70 +35,56 @@ public class HomeController {
 	private DAOAssunto daoAssunto;
 	@Autowired
 	private DAOPermissaoUsuario daoPermissaoUsuario;
-	
+
 	public DAOPermissaoUsuario getDaoPermissaoUsuario() {
 		return daoPermissaoUsuario;
 	}
-	
-	public void setDaoPermissaoUsuario(DAOPermissaoUsuario dao) {
-		daoPermissaoUsuario = dao;
-	}
-	
+
 	public DAOUsuario getDaoUsuario() {
 		return daoUsuario;
-	}
-
-	public void setDaoUsuario(DAOUsuario daoUsuario) {
-		this.daoUsuario = daoUsuario;
 	}
 
 	public DAOTopico getDaoTopico() {
 		return daoTopico;
 	}
 
-	public void setDaoTopico(DAOTopico daoTopico) {
-		this.daoTopico = daoTopico;
-	}
-
 	public DAOAssunto getDaoAssunto() {
 		return daoAssunto;
 	}
 
-	public void setDaoAssunto(MockDAOAssunto daoAssunto) {
-		this.daoAssunto = daoAssunto;
-	}
-	
 	/**
-	 * A anotação @RequestMapping identifica qual a URL relacionada ao método (action)
-	 * a ser executado.
+	 * A anotação @RequestMapping identifica qual a URL relacionada ao método
+	 * (action) a ser executado.
 	 * 
-	 * Neste exemplo, vemos que a URL padrão para nosso sistema, o "/" sempre apontará para 
-	 * esta chamada.
+	 * Neste exemplo, vemos que a URL padrão para nosso sistema, o "/" sempre
+	 * apontará para esta chamada.
+	 * 
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping("/")
 	public String index(Map<String, Object> model) {
-		model.put("assuntos", getDaoAssunto().list(0,100));
-		model.put("usuarios", getDaoUsuario().list(0,100));
+		model.put("assuntos", getDaoAssunto().list(0, 100));
+		model.put("usuarios", getDaoUsuario().list(0, 100));
 		return "index";
 	}
-	
+
 	@RequestMapping("/registro")
 	public String registro(Map<String, Object> model) {
 		if (model.get("usuario") == null) {
 			Usuario usr = new Usuario();
-			
+
 			model.put("usuario", usr);
 		}
 		return "registro";
 	}
-	
-	@RequestMapping(value="/executarRegistro", method=RequestMethod.POST)
-	public String executarRegistro(@Valid Usuario usuario, 
-									BindingResult bindingResult, 
-									HttpSession sessao, 
-									@RequestParam(value="avatar", required=false) MultipartFile avatar) {
+
+	@RequestMapping(value = "/executarRegistro", method = RequestMethod.POST)
+	public String executarRegistro(
+			@Valid Usuario usuario,
+			BindingResult bindingResult,
+			HttpSession sessao,
+			@RequestParam(value = "avatar", required = false) MultipartFile avatar) {
 		if (bindingResult.hasErrors()) {
 			Map<String, Object> model = new HashMap<String, Object>();
 			model.put("usuario", usuario);
@@ -107,27 +92,29 @@ public class HomeController {
 		}
 		getDaoUsuario().persistir(usuario);
 		getDaoPermissaoUsuario().addRole("ROLE_MEMBRO", usuario);
-		if (! avatar.isEmpty()) {
+		if (!avatar.isEmpty()) {
 			processarAvatar(usuario, avatar);
 		}
-		
+
 		sessao.setAttribute("usuario", usuario);
 		return "redirect:/";
 	}
-	
+
 	private void processarAvatar(Usuario usuario, MultipartFile avatar) {
 		File diretorio = new File("/springForum/avatares");
-		if (! diretorio.exists()) {
+		if (!diretorio.exists()) {
 			diretorio.mkdirs();
 		}
-			try {
-				FileOutputStream arquivo = new FileOutputStream(diretorio.getAbsolutePath() + "/" + usuario.getLogin() + ".png");
-				arquivo.write(avatar.getBytes());
-				arquivo.close();
-			} catch (IOException ex) {
-				
-			}
-		
+		try {
+			FileOutputStream arquivo = new FileOutputStream(
+					diretorio.getAbsolutePath() + "/" + usuario.getLogin()
+							+ ".png");
+			arquivo.write(avatar.getBytes());
+			arquivo.close();
+		} catch (IOException ex) {
+
+		}
+
 	}
-	
+
 }
